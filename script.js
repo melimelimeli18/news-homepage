3832ae402bfac0dabb6870195a53192f1aeda1aa
melisaolivia <melisaolivia18@gmail.com>
2023-02-22 17:21:22 +0700

    adding the mobile-menu

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    const humbergerMenu = document.querySelector(".hamburger-menu");
+    const navbar = document.querySelector(".navigation-bar");
+
+    humbergerMenu.addEventListener("click", function(){
+        navbar.classList.toggle('show'); //slide responsive
+        
+    });
+
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 767) {
+        navbar.classList.remove('show');
+        }
+    });
+})
